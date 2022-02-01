1a7adac126177c8d9fd85486d5408ff7fcc0664a
YanaSheri <yana.shereheda@gmail.com>
2022-02-02 00:53:40 +0200

    Add styles to Statistics component

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,12 +1,20 @@
 import PropTypes from "prop-types";
+import s from './Statistics.module.css';
+
 const Statistics = ({ stats }) => {
-    return <section className="statistics">
-    <h2 className="title">Upload stats</h2>
-    <ul className="stat-list">
+    return <section className={s["statistics"]}>
+    <h2 className={s["title"]}>Upload stats</h2>
+    <ul className={s["statList"]}>
         {stats.map(stat => (
-            <li className="item" key={stat.id}>
-                <span className="label">{stat.label}</span>
-                <span className="percentage">{stat.percentage}</span>
+            <li className={s["item"]} key={stat.id}
+            style={{
+                backgroundColor: `#${Math.floor(Math.random() * 16777215).toString(
+                16,
+                )}`,
+            }}
+            >
+                <span className={s["label"]}>{stat.label} </span>
+                <span className={s["percentage"]}>{stat.percentage}%</span>
             </li>
             ))
         }
