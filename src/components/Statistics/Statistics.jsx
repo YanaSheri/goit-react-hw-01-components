c75145592ce95752d72003583ccb8294ad74f8a5
YanaSheri <yana.shereheda@gmail.com>
2022-02-03 11:28:47 +0200

    Update

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,8 +3,8 @@ import s from './Statistics.module.css';
 
 const Statistics = ({ stats, title }) => {
     return <section className={s["statistics"]}>
-        {/* {title ? <h2 className={s["title"]}>Upload stats</h2> : null} */}
-    <h2 className={s["title"]}>{title ? "Upload stats" : null}</h2>
+        {title ? <h2 className={s["title"]}>{ title }</h2> : null}
+    {/* <h2 className={s["title"]}>{title ? "Upload stats" : null}</h2> */}
     <ul className={s["statList"]}>
         {stats.map(stat => (
             <li className={s["item"]} key={stat.id}
